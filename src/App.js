ca4e72651d5743dc6ad53a676c986d41720fb166
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-04-10 13:08:48 -0400

    Add the verses of the tablet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./app.css";
+import { verses, baha } from "./tablet";
 
 function App() {
+  const [tablet, setTablet] = useState([]);
+  const [verse, setVerse] = useState(0);
+
+  useEffect(() => {
+    setTablet(verses);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app__header">
+        <h1>Fire Tablet</h1>
+        <h4 className="app__baha">{baha}</h4>
       </header>
+      <main className="app__main">{tablet[0]}</main>
+      <footer className="app__footer">
+        &copy; {new Date().getFullYear()} Vincent Ramdhanie
+      </footer>
     </div>
   );
 }
