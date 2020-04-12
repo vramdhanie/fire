66adc7c99f01ca478fa79e8131d2926a544aacbb
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-04-12 13:00:52 -0400

    Updates

---
+++ precache-manifest.daff2ec0d51d9ca699ce8db7c271c871.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084ed1857cb7b48edfd36c032a9f905b",
+    "url": "/fire/index.html"
+  },
+  {
+    "revision": "2162b07344659b5a05fa",
+    "url": "/fire/static/css/main.119390ab.chunk.css"
+  },
+  {
+    "revision": "cc5c008ae52d747d3ccc",
+    "url": "/fire/static/js/2.8efa7b94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fire/static/js/2.8efa7b94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2162b07344659b5a05fa",
+    "url": "/fire/static/js/main.e02bda6b.chunk.js"
+  },
+  {
+    "revision": "1e80deb3cbac6e2e2055",
+    "url": "/fire/static/js/runtime-main.b194a9bf.js"
+  }
+]);
